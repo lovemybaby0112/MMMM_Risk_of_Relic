5a55286860ed12197416fda9d6394f8e0f00ecf1
zx987789xz <93311105+zx987789xz@users.noreply.github.com>
2022-01-24 21:34:06 +0800

    attack0.2(ray)

--- Assets/_Custom/Scripts/Attack.cs
+++ Assets/_Custom/Scripts/Attack.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using StarterAssets;
+using UnityEngine.InputSystem;
 
 public class Attack : MonoBehaviour
 {
-     public Transform player;
-    // Start is called before the first frame update
+    public Transform cinemachine;
+
+    [SerializeField]
+    private LayerMask attackColliderLayerMask = new LayerMask();
+    [SerializeField]
+    private Transform debugTran;
+
+    private ThirdPersonController ThirdPersonController;
+    private StarterAssetsInputs StarterAssetsInputs;
+    Vector2 screenCenterPoint = new Vector2(Screen.width / 2, Screen.height / 2);
+    Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+    private void Awake()
+    {
+        //StarterAssetsInputs = GetComponent<StarterAssetsInputs>();
+        //ThirdPersonController = GetComponent<ThirdPersonController>();
+    }
     void Start()
     {
         
@@ -14,13 +31,27 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 forword = transform.TransformDirection
-        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if (Input.GetButton("Fire1"))
         {
-            Debug.DrawRay(ray,0.1f);
+            Shoot();
+            
         }
+        Debug.DrawLine(cinemachine.position, screenCenterPoint, Color.red, 0.1f);
+        Debug.DrawRay(ray.origin, ray.direction, Color.red, 0.1f);
+    }
 
+    void Shoot()
+    {
+        RaycastHit Hit;
+
+        Physics.Raycast(ray, out Hit, 999f);
+        
+            GameObject hitObj = Hit.collider.gameObject;
+            Debug.Log(hitObj);
+        
+
+        
+        
+        
     }
 }
